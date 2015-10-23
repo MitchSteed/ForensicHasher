06e76a71b829ba1a1d81f20982843b1f55b69803
Mitchell Steed <mitchell.steed@gmail.com>
2015-10-23 15:12:16 -0600

    Basic layout and structure for victim and workstation

---
+++ victim.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+
+
+
+
+def getData():
+    return sys.stdin.read()
+
+
+
+def parse_arguments():
+    ''' parse arguments, which include '-p' for port and '-h' for host'''
+    parser = argparse.ArgumentParser(prog='Forensic Hasher', description='A simple tool to date, and hash linux command outputs', add_help=True)
+    parser.add_argument('-p', '--port', type=int, action='store', help='port the victim will bind to', default=3000)
+    parser.add_argument('-h', '--host', type=str, action='store', help='host the victim will connect to', default='localhost')
+    parser.add_argument('-n', '--name', type=str, action='store', help='filename to put the output in', default='ForensicHasher.txt')
+    args = parser.parse_args()
+
+    return args.host, args.port, args.name
+
+
+def send(host, port, name, data):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    msg = data + "%%%%" + name
+    msglen = len(msg)
+    totalsent = 0
+    while totalsent < msglen:
+        sent = sock.send(msg[totalsent:])
+        totalsent += sent
+    sock.close()
+
+
+def run():
+    host, port, name = parse_arguments()
+    data = getData()
+    send(host, port, name, data)
+
+
+
+if __name__ == "__main__":
+    run()
+    
---
+++ workstation.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+import datetime
+import socket
+import sys
+
+buff_size = 1024
+hash_algorithm = "md5"
+data_delim = "%%%%"
+
+
+def getData():
+    return sys.stdin.read()
+
+
+
+def parse_arguments():
+    ''' parse arguments, which include '-p' for port and '-h' for host'''
+    parser = argparse.ArgumentParser(prog='Forensic Hasher', description='A simple tool to date, and hash linux command outputs', add_help=True)
+    parser.add_argument('-p', '--port', type=int, action='store', help='port the victim will listen on', default=3000)
+    parser.add_argument('-a', '--algorithm', type=str, action='store', 
+        help='hashing algorith to use, options include: {}'.format(hashlib.algorithms), default='md5')
+    args = parser.parse_args()
+
+    return args.port, args.algoithm.lower()
+
+def getContentsAndFileName(data):
+    split_list = data.split(data_delim)
+    file_name = split_list[-1]
+    file_contents = data_delim.join(split_list[:-1])
+    return file_name, file_contents
+
+def getDate():
+    datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+def getHash(hashable):
+    method = getattr(hashlib, hash_algorithm)
+    method.update(hashable)
+    return method.hexdigest()
+
+
+def writeFile(file_name, file_contents):
+    with open(file_name, "w+") as f:
+        f.write(file_contents)
+
+def writeFiles(data):
+
+    # main file and hash
+    file_name, file_contents = getContentsAndFileName(data)
+    file_hash = getHash(file_contents)
+    writeFile(file_name, file_contents)
+    writeFile(file_name +"." + hash_algorithm, file_hash)
+    
+    # date and date hash
+    date = getDate()
+    date_hash = getHash(date)
+    writeFile(file_name + ".date", date)
+    writeFile(file_name + ".date." + hash_algorithm, date_hash)
+
+
+
+def handle(client):
+    data = ""
+    while True:
+        localData = client.recv(buff_size)
+        if localData:
+            data += localData
+        else:
+            client.close()
+            break
+
+    writeFiles(data)
+
+def listen(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+    sock.bind(("", port))
+    sock.listen(5)
+    while True:
+        client, _ = sock.accept()
+        handle(client)
+
+    sock.close()
+
+
+def run():
+    port, hash_algorithm = parse_arguments()
+    data = getData()
+    send(host, port, name, data)
+
+
+
+if __name__ == "__main__":
+    run()
+    
