2c2cd06208f0e69cb66aa557025bf6d7d367efc5
Douglas Kelly <dkelly@prosper.com>
2015-10-29 15:13:21 -0600

    Updating Victim.py's documentation

--- victim.py
+++ victim.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 
+'''
+'   Authors: Mitchell Steed
+'   Contributing Author: Douglas Kelly
+'   This file is meant to be ran on a victim machine. It pipes the content of Standard Out to the socket that this file opens
+'   with the example: ls | python victim.py
+'''
+
+'Import argument Parser, a library used for generating easy documentation based on expected parameter flags
 import argparse
+'Imports the socket library, used to establish a tunnel between the bictim and host machines
 import socket
+'Imports the system library, used for interacting with the underlying operating system
 import sys
 
-
-
-
+'Retrieves the Data from Standard In
 def getData():
     return sys.stdin.read()
 
-
-
+'Parses the incoming arguments, or if none are provided provides content regarding which flags are available
 def parse_arguments():
-    ''' parse arguments, which include '-p' for port and '-h' for host'''
+    ''' parse arguments, which include '-p' for port and '-d' for host'''
     parser = argparse.ArgumentParser(prog='Forensic Hasher', description='A simple tool to date, and hash linux command outputs', add_help=True)
     parser.add_argument('-p', '--port', type=int, action='store', help='port the victim will bind to', default=3000)
     parser.add_argument('-d', '--destination', type=str, action='store', help='destination host the victim will connect to', default='localhost')
@@ -22,7 +29,7 @@ def parse_arguments():
 
     return args.destination, args.port, args.name
 
-
+'Sends the data to the established connection port on the Host machine
 def send(host, port, name, data):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host, port))
@@ -34,14 +41,12 @@ def send(host, port, name, data):
         totalsent += sent
     sock.close()
 
-
+'Function called at runtime, clean separation of running function from main
 def run():
     host, port, name = parse_arguments()
     data = getData()
     send(host, port, name, data)
-
-
-
+    
+'The Main method for Python programs as a best practice, clear code start
 if __name__ == "__main__":
     run()
-    
