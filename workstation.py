a88d4f99b7b294fe8e74c125c6d8a9e9e640b6c6
Douglas Kelly <dkelly@prosper.com>
2015-10-29 15:30:16 -0600

    updating documentation for host

--- workstation.py
+++ workstation.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 
+'''
+'   Author: Mitchell Steed
+'   Contributing Author: Douglas Kelly
+'   This file opens up a socket on the Host machine and allows the standard in from the Victim to be piped to it.
+'''
+
+'Import argument Parser, a library used for generating easy documentation based on expected parameter flags
 import argparse
+'Imports the datetime library used for disecting and interpreting datetime formats
 import datetime
+'Imports the hashlib library, used for creation of hashes based on the content imported from the socket
 import hashlib
+'Imports the socket library, used to establish a tunnel between the bictim and host machines
 import socket
 
+'Set program constants
 buff_size = 1024
 hash_algorithm = "md5"
 data_delim = "%%%%"
 
-
 def parse_arguments():
-    ''' parse arguments, which include '-p' for port and '-h' for host'''
+    ''' parse arguments, which include '-p' for port and '-d' for host'''
     parser = argparse.ArgumentParser(prog='Forensic Hasher', description='A simple tool to date, and hash linux command outputs', add_help=True)
     parser.add_argument('-p', '--port', type=int, action='store', help='port the victim will listen on', default=3000)
     parser.add_argument('-a', '--algorithm', type=str, action='store',
@@ -21,15 +31,18 @@ def parse_arguments():
     return args.port, args.algorithm.lower()
 
 def getContentsAndFileName(data):
+    '' Receives a data string parameter and returns a file name along with the contents of the file being the data
     split_list = data.split(data_delim)
     file_name = split_list[-1]
     file_contents = data_delim.join(split_list[:-1])
     return file_name, file_contents
 
 def getDate():
+    '' Returns the current datetime of the imported file
     return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 def getHash(hashable):
+    '' Returns the hash of the content delivered to this method as a String parameter
     method = getattr(hashlib, hash_algorithm)
     h = method()
     h.update(hashable)
@@ -37,11 +50,15 @@ def getHash(hashable):
 
 
 def writeFile(file_name, file_contents):
+    '' Writes the file to the system
     with open(file_name, "w+") as f:
         f.write(file_contents)
 
 def writeFiles(data):
-
+    ''' This is the main function called when content is received via the socket. 
+        Takes the string data, generate the file, then gives the file to the hasher to create a hash
+        Also prints out the datettime of the file, along with its hash
+    '''
     # main file and hash
     file_name, file_contents = getContentsAndFileName(data)
     file_hash = getHash(file_contents)
@@ -54,9 +71,8 @@ def writeFiles(data):
     writeFile(file_name + ".date", date)
     writeFile(file_name + ".date." + hash_algorithm, date_hash)
 
-
-
 def handle(client):
+    '' 
     data = ""
     while True:
         localData = client.recv(buff_size)
@@ -69,6 +85,7 @@ def handle(client):
     writeFiles(data)
 
 def listen(port):
+    '' Main function called from the run method
     print "Now listening for input: "
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
@@ -81,10 +98,10 @@ def listen(port):
     sock.close()
 
 def run():
+    '' Grabs the arguments input by the user and stores them to the global variables to be used by the listen function
     port, hash_algorithm = parse_arguments()
     listen(port)
 
-
-
 if __name__ == "__main__":
+    '' Main method defined for the python file by convention
     run()
